93cac2f7f853724fd6209e0864d303c31d644c4d
Atiba <atibaanwar18@gmail.com>
2024-01-28 11:35:52 +0530

    blogs style added

--- src/main/java/com/blog/dev/BlogController.java
+++ src/main/java/com/blog/dev/BlogController.java
@@ -2,24 +2,22 @@ package com.blog.dev;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class BlogController {
-    @GetMapping({"/","/blogs"})
+    @GetMapping({ "/", "/blogs" })
     public String blogs() {
         return "blogs";
     }
+
     @GetMapping("/blog")
     public String blog() {
         return "blog";
     }
+
     @GetMapping("/add-blog")
     public String addBlog() {
         return "add-blog";
     }
 
-    
-    
 }
